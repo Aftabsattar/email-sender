8301e6794cdd1210ea189b143b6997bbbee7c584
Muhammad Aftab <71002@gcslahore.edu.pk>
2024-12-07 00:43:28 +0500

    just practice project:

---
+++ sender-email/src/app/page.tsx
@@ -0,0 +1,10 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <>
+    <h1>hy pakistan</h1>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Necessitatibus delectus recusandae odit molestias voluptatibus nemo obcaecati sapiente vitae inventore, labore eligendi iure alias!</p>
+    </>
+  );
+}
